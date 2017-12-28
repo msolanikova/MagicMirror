09009e221411ab480c8886e6f96202f3daad29ef
Marketa Solanikova <solanikova@promark.sk>
2017-12-28 20:15:10 +0100

    startup script

--- startup/start.sh
+++ startup/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/pi/Development/MagicMirror
-node serveronly
-DISPLAY=:0 chromium-browser -kiosk "http://localhost:8080"
+node serveronly & DISPLAY=:0 chromium-browser -kiosk "http://localhost:8080"
